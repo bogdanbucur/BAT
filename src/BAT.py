11e82f890aeaeb9b8ca3dfdf7dbc9cb4852605c0
Bogdan Bucur <bogdan.bucur@udevoffice.ro>
2017-07-03 18:55:44 +0300

    maybe done, maybe not

--- src/BAT.py
+++ src/BAT.py
@@ -10,7 +10,7 @@ class BAT(unittest.TestCase):
             'platformName': 'Android',
             'platformVersion': '6.0',
             'deviceName': 'Nexus 9 API 22',
-            'app': '/Users/bogdanbucur/PycharmProjects/BAT/src/BATLauncher27.06.2017.apk',
+            'app': '/Users/bogdanbucur/PycharmProjects/BAT/src/BAT.apk',
             'appPackage': 'com.udevoffice.Launcher'
         }
 
@@ -18,21 +18,19 @@ class BAT(unittest.TestCase):
 
     def test(self):
 
-        for i in range(2):
+        for i in range(8):
             try:
                 # Input Username
                 userField = self.driver.find_element_by_id('com.udevoffice.Launcher:id/username')
                 userField.click()
                 userField.send_keys('test')
-                self.driver.press_keycode('66')
+                self.driver.back()
 
                 # Input Password
                 passField = self.driver.find_element_by_id('com.udevoffice.Launcher:id/password')
                 passField.click()
                 passField.send_keys('test')
-                self.driver.press_keycode('66')
-
-                self.driver.hide_keyboard()
+                self.driver.back()
 
                 # Login
                 self.driver.find_element_by_id('com.udevoffice.Launcher:id/loginButton').click()
@@ -49,7 +47,7 @@ class BAT(unittest.TestCase):
                 # Stoc Dunhill FC
                 self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
                                                   '/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
-                                                  '/android.widget.TextView[1]').click()
+                                                  '/android.widget.TextView[contains(@index, "1")]').click()
                 dunhillFCStocPachet = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
                                                                         '/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]'
                                                                         '/android.widget.EditText[1]')
@@ -57,6 +55,7 @@ class BAT(unittest.TestCase):
                 dunhillFCStocPachet.clear()
                 dunhillFCStocPachet.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 dunhillFCStocApaTermala = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]'
                                                                             '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
@@ -65,6 +64,7 @@ class BAT(unittest.TestCase):
                 dunhillFCStocApaTermala.clear()
                 dunhillFCStocApaTermala.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 dunhillFCStocCremaDeSoare = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]'
                                                                               '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
@@ -73,17 +73,18 @@ class BAT(unittest.TestCase):
                 dunhillFCStocCremaDeSoare.clear()
                 dunhillFCStocCremaDeSoare.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 # Stoc PallMall
-                self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
-                                                  '/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
-                                                  '/android.widget.TextView').click()
+                self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]'
+                                                  '/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.TextView').click()
                 pallmallStocBricheta = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
-                                                                         '/android.widget.LinearLayout[2]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout/android.widget.EditText')
+                                                                         '/android.widget.LinearLayout[2]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.EditText')
                 pallmallStocBricheta.click()
                 pallmallStocBricheta.clear()
                 pallmallStocBricheta.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 pallmallStocPachet = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
                                                                        '/android.widget.LinearLayout[2]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.EditText')
@@ -91,15 +92,19 @@ class BAT(unittest.TestCase):
                 pallmallStocPachet.clear()
                 pallmallStocPachet.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 # Stoc Dunhill FS
+                self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
+                                                  '/android.widget.LinearLayout[3]/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
+                                                  '/android.widget.TextView[contains(@index, "1")]').click()
                 dunhillFSStoc = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
-                                                                  '/android.widget.LinearLayout[3]/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
-                                                                  '/android.widget.TextView[1]')
+                                                                  '/android.widget.LinearLayout[3]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout/android.widget.EditText')
                 dunhillFSStoc.click()
                 dunhillFSStoc.clear()
                 dunhillFSStoc.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 # Stoc Kent
                 kentStocBricheta = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
@@ -108,6 +113,7 @@ class BAT(unittest.TestCase):
                 kentStocBricheta.clear()
                 kentStocBricheta.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 kentStocPachet = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
                                                                    '/android.widget.LinearLayout[4]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.EditText')
@@ -115,22 +121,27 @@ class BAT(unittest.TestCase):
                 kentStocPachet.clear()
                 kentStocPachet.send_keys('1000')
                 self.driver.back()
+                sleep(1)
+
+                self.driver.scroll(dunhillFSStoc, dunhillFCStocPachet)
 
                 # Stoc Vogue
                 vogueStocPachet = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]'
-                                                                         '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[5]/android.support.v7.widget.RecyclerView'
-                                                                         '/android.widget.LinearLayout[1]/android.widget.EditText')
+                                                                    '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[5]/android.support.v7.widget.RecyclerView'
+                                                                    '/android.widget.LinearLayout[1]/android.widget.EditText')
                 vogueStocPachet.click()
                 vogueStocPachet.clear()
                 vogueStocPachet.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 vogueStocTermala = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView'
-                                                                       '/android.widget.LinearLayout[5]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.EditText')
+                                                                     '/android.widget.LinearLayout[5]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.EditText')
                 vogueStocTermala.click()
                 vogueStocTermala.clear()
                 vogueStocTermala.send_keys('1000')
                 self.driver.back()
+                sleep(1)
 
                 # Save Stoc
                 self.driver.find_element_by_id('com.udevoffice.Launcher:id/save').click()
@@ -141,7 +152,7 @@ class BAT(unittest.TestCase):
                 self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout'
                                                   '/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.TextView[contains(@index, "{0}")]'.format(str(index))).click()
                 self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]'
-                                                  '/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]').click()
+                                                  '/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.ImageView').click()
                 sleep(2)
                 self.driver.find_element_by_id('com.udevoffice.Launcher:id/bestProductImg').click()
 
@@ -150,17 +161,71 @@ class BAT(unittest.TestCase):
                 phoneField = self.driver.find_element_by_id('com.udevoffice.Launcher:id/phone')
                 phoneField.click()
                 phoneField.send_keys('0732875621')
-                self.driver.press_keycode('66')
+                self.driver.back()
+                sleep(3)
 
                 # Send Code
                 self.driver.find_element_by_id('com.udevoffice.Launcher:id/sendPhoneButton').click()
 
-            except:
-                pass
+                # Verify Code
+                sleep(3)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/sendConfirmationButton').click()
 
-            # index = i % 8
-            # self.driver.find_elements_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout'
-            #                                    '/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.TextView[contains(@index, {0})]'.format(str(index)))
+                # Assign prize
+                sleep(2)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/giveAway').click()
+
+                # Skip error
+                try:
+                    self.driver.find_element_by_id('android:id/button1').click()
+                except:
+                    pass
+
+                # Restart
+                sleep(2)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/restart').click()
+                sleep(2)
+
+            except:
+
+                # Iterate First 8 Brands
+                index = i % 8
+                self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout'
+                                                  '/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.TextView[contains(@index, "{0}")]'.format(str(index))).click()
+                self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]'
+                                                  '/android.support.v7.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.ImageView').click()
+                sleep(2)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/bestProductImg').click()
+
+                # Input Phone Number
+                sleep(2)
+                phoneField = self.driver.find_element_by_id('com.udevoffice.Launcher:id/phone')
+                phoneField.click()
+                phoneField.send_keys('0732875621')
+                self.driver.back()
+                sleep(3)
+
+                # Send Code
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/sendPhoneButton').click()
+
+                # Verify Code
+                sleep(3)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/sendConfirmationButton').click()
+
+                # Assign prize
+                sleep(2)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/giveAway').click()
+
+                # Skip error
+                try:
+                    self.driver.find_element_by_id('android:id/button1').click()
+                except:
+                    pass
+
+                # Restart
+                sleep(2)
+                self.driver.find_element_by_id('com.udevoffice.Launcher:id/restart').click()
+                sleep(2)
 
 
 if __name__ == '__main__':
